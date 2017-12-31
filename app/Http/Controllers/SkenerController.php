785766ac3b6c76460bf039bda070fb5a03e7073e
Ezols <Edvards.ozols@gmail.com>
2017-12-31 18:41:51 +0200

    Lexer completed

--- app/Http/Controllers/SkenerController.php
+++ app/Http/Controllers/SkenerController.php
@@ -1,158 +1,234 @@
 <?php namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Log;
 
 class SkenerController
 {
-    const DELIM = ['.',';',':','[',']','=','#','(',')','/','*','-','+','%','^'];
+    const DELIM = ['.',',',';',':','[',']','=','#','(',')','/','*','-','+','%','^'];
     const DELIM2 = ['..',':='];
-    const KEYWORDS = ['constructor', 'const', 'array', 'of', 'Char', 'var', 'Assign', 'Begin', 'Init', 'or', 'for', 'do', 'New', 'Insert', 'end', 'Integer', 'Pview', 'TRect', 'inherited',  'and', 'not']; 
-    const CODE = "constructor TCalculator.Init;
+    const KEYWORDS = ['constructor', 'const', 'array', 'of', 'Char', 'var', 'Assign', 'Begin', 'Init', 'or', 'for', 'do', 'New', 'Insert', 'end', 'Integer', 'Pview', 'TRect', 'inherited',  'and', 'not'];
+    const CODE = " constructor TCalculator.Init;
     const KeyChar: array[0..19] of Char = 'C'#27'%'#241'789/456*123-0.=+';
     var I: Integer; P: PView; R: TRect;
     Begin
-    R.Assign(5, 3, 29, 18); inherited Init(R, 'Calculator');
+    R.Assign(5, 3, 29, 18); inherited Init(R?, 'Calculator');
     Options := Options or ofFirstClick;
     for I := 0 to 19 do P:=New(PButton, cmCalcButton);
     P^.Options := P^.Options and not ofSelectable;
     Insert(P);
     R.Assign(3, 2, 21, 3);
     Insert(New(PCalcDisplay, Init(R)));
-    End; ";
-    
-        const FAKE_CODE = ".";
-    protected $multicharacters = [];
+    End;";
 
-    public function __construct()
-    {
-        $this->multicharacters = array_merge(static::DELIM2, static::KEYWORDS);
-    }
+    protected $tokens = [];
+    protected $lines = 1;
+    protected $position = 1;
 
-    // static::DELIM;
     public function skener()
     {
-        //echo(implode(" | ", static::DELIM));
-
-        // converted CODE to array
-        $response = $this->codeParser(static::CODE);
-
-        echo "<pre>";
-        print_r($response);
-        echo "</pre>";
+        $code = str_split(static::CODE);
+        return $this->tokenize($code);
     }
 
-    protected function codeParser($code)
+    protected function tokenize($chars)
     {
-        $code = str_split($code);
-        $inProgress = "";
-        $response = [];
-
-        foreach($code as $char)
+        Log::info(head($chars) . " | " . count($chars));
+        switch (true)
         {
-            if($this->isWhitespaceCharacter($char))
-            {
-                // Parbaudit in progress
-                continue;
-            }
+            // TO DO check if chars is empty
+            case count($chars) === 0:
+                return $this->tokens;
 
-            
-            $isMatch = $this->isDelimOrKeyword($char);
-            $inProgress = $inProgress . $char;
+            case $this->isWhitespace($chars):
+                $chars = $this->parseWhitespace($chars);
+                break;
 
-            if($this->maybeDelimOrKeyword($inProgress))
-            {
-                continue;
-            }
+            case $this->isLetter($chars):
+                $chars = $this->parseLetters($chars);
+                break;
 
-            $isMatch = $this->isDelimOrKeyword($inProgress);
+            case $this->isDelim2($chars):
+                $chars = $this->parseDelim2($chars);
+                break;
 
-            if($isMatch)
-            {
-                $response[] = ['symbol' => $inProgress, 'type' => $isMatch];
-                $inProgress = "";
-            }
+            case $this->isDelim($chars):
+                $chars = $this->parseDelim($chars);
+                break;
 
-            // if($this->isDelim2($inProgress)) {
-            //     // TODO": its delim2
-            //     $response[] = ['symbol' => $char, 'type' => 'delim2'];
-            //     continue;
-            // }
+            case $this->isNumber($chars):
+                $chars = $this->parseNumber($chars);
+                break;
 
-            // if($this->canBeDelim2($inProgress)) 
-            // {
-            //     continue;
-            // }
+            case $this->isComment($chars):
+                $chars = $this->parseComment($chars);
+                break;
 
-            // if($this->isDelim($char))
-            // {
-            //     $response[] = ['symbol' => $char, 'type' => 'delim'];
-            // }
-            
-        }
-        return $response;
-    }
-
-    protected function isDelim($char)
-    {
-        return in_array($char, static::DELIM);
-    }
-
-    protected function isDelim2($char)
-    {
-        return in_array($char, static::DELIM2);
-    }
-
-    protected function canBeDelim2($char)
-    {
-        foreach(static::DELIM2 as $delim2)    
-        {
-            $pos = strpos($delim2, $char);
-            if($pos === 0)
-            {
-                return true;
-            }
+            default:
+                throw new LexerException($chars[0], ceil($this->lines / 2), $this->position);
 
         }
-        return false;
+
+        return $this->tokenize($chars);
     }
 
-    protected function maybeDelimOrKeyword($inProgress): bool
+    protected function foundToken($type, $value)
     {
-        foreach($this->multicharacters as $keywordDelim2)
-        {
-            $isStartingWith = strpos($keywordDelim2, $inProgress) === 0;
-            $isEqual = $inProgress == $keywordDelim2;
-
-            if($isStartingWith && !$isEqual)
-            {
-                return true;
-            }
-        }
-        return false;
+        $this->tokens[] = ['type' => $type, 'value' => $value];
     }
 
-    protected function isDelimOrKeyword($inProgress)
+    protected function charsOffset($chars, $offSet = 1)
     {
-        if(in_array($inProgress, static::DELIM))
-        {
-            return "DELIM";
-        }
-
-        if(in_array($inProgress, static::DELIM2))
-        {
-            return "DELIM2";
-        }
-
-        if(in_array($inProgress, static::KEYWORDS))
-        {
-            return "KEYWORD";
-        }
-
-        return false;
+        $this->position += $offSet;
+        return array_slice($chars, $offSet);
     }
 
-    protected function isWhitespaceCharacter($code)
+    protected function splitWhile($chars, $callback)
     {
-        return $code === " ";
+        $ok = [];
+        $i = 0;
+        do
+        {
+            $ok[] = $chars[$i];
+            $i++;
+
+        } while($callback($chars, $i));
+
+        $rest = $this->charsOffset($chars, $i);
+
+        return [$ok, $rest];
+    }
+
+    protected function parseWhitespace($chars)
+    {
+        $responseChars = $this->charsOffset($chars);
+
+        $char = $chars[0];
+        $newLines = ["\r\n", "\n", "\r"];
+        if(in_array($char, $newLines))
+        {
+            $this->lines++;
+            $this->position = 1;
+        }
+
+        return $responseChars;
+    }
+
+    protected function parseLetters($chars)
+    {
+        $callback = function($chars, $index)
+        {
+            return $this->isLetter($chars, $index);
+        };
+
+        list($letters, $chars) = $this->splitWhile($chars, $callback);
+
+        $letters = join("", $letters);
+
+        if(in_array($letters, static::KEYWORDS))
+        {
+            $this->foundToken("keyword", $letters);
+        }
+        else
+        {
+            $this->foundToken("id", $letters);
+        }
+
+        return $chars;
+    }
+
+    protected function parseDelim($chars)
+    {
+        $this->foundToken("delim", $chars[0]);
+
+        return $this->charsOffset($chars);
+    }
+
+    protected function parseDelim2($chars)
+    {
+        $delim2 = $chars[0] . $chars[1];
+        $this->foundToken("delim2", $delim2);
+
+        return $this->charsOffset($chars, 2);
+    }
+
+    protected function parseNumber($chars)
+    {
+        $callback = function($chars, $index)
+        {
+            return $this->isNumber($chars, $index);
+        };
+
+        list($numbers, $chars) = $this->splitWhile($chars, $callback);
+
+        $numbers = join("", $numbers);
+
+        $this->foundToken("literal", $numbers);
+
+        return $chars;
+    }
+
+    protected function parseComment($chars)
+    {
+        $callback = function($chars, $index)
+        {
+            return !$this->isComment($chars, $index);
+        };
+
+        list($comment, $chars) = $this->splitWhile($chars, $callback);
+
+        $comment[] = array_shift($chars);
+
+        $comment = join("", $comment);
+
+        $this->foundToken("comment", $comment);
+
+        return $chars;
+    }
+
+    protected function isWhitespace($chars, $index = 0)
+    {
+        $whiteSpaces = [" ", "\r\n", "\t", "\n", "\r"];
+        return in_array($chars[$index], $whiteSpaces);
+    }
+
+    protected function isLetter($chars, $index = 0)
+    {
+        return ctype_alpha($chars[$index]);
+    }
+
+    protected function isDelim2($chars, $index = 0)
+    {
+        if(count($chars) < 2)
+        {
+            return false;
+        }
+
+        $firstTwo = $chars[$index] . $chars[$index + 1];
+        return in_array($firstTwo, static::DELIM2);
+
+    }
+
+    protected function isDelim($chars, $index = 0)
+    {
+        return in_array($chars[$index], static::DELIM);
+    }
+
+    protected function isNumber($chars, $index = 0)
+    {
+        return ctype_digit($chars[$index]);
+    }
+
+    protected function isComment($chars, $index = 0)
+    {
+        return $chars[$index] === "'";
+    }
+}
+
+class LexerException extends \Exception
+{
+    public function __construct($char, $line, $position)
+    {
+        parent::__construct("found unexpected character: [$char]. At: [$line]:[$position]");
     }
 }
