3d3062875ffa0534f3228cd831aeb3f30e15ea0a
Ezols <Edvards.ozols@gmail.com>
2017-12-31 14:04:55 +0200

    Old lekser

--- app/Http/Controllers/SkenerController.php
+++ app/Http/Controllers/SkenerController.php
@@ -7,7 +7,7 @@ class SkenerController
     const DELIM = ['.',';',':','[',']','=','#','(',')','/','*','-','+','%','^'];
     const DELIM2 = ['..',':='];
     const KEYWORDS = ['constructor', 'const', 'array', 'of', 'Char', 'var', 'Assign', 'Begin', 'Init', 'or', 'for', 'do', 'New', 'Insert', 'end', 'Integer', 'Pview', 'TRect', 'inherited',  'and', 'not']; 
-    const CODE = ".constructor TCalculator.Init;
+    const CODE = "constructor TCalculator.Init;
     const KeyChar: array[0..19] of Char = 'C'#27'%'#241'789/456*123-0.=+';
     var I: Integer; P: PView; R: TRect;
     Begin
@@ -19,8 +19,14 @@ class SkenerController
     R.Assign(3, 2, 21, 3);
     Insert(New(PCalcDisplay, Init(R)));
     End; ";
+    
         const FAKE_CODE = ".";
+    protected $multicharacters = [];
 
+    public function __construct()
+    {
+        $this->multicharacters = array_merge(static::DELIM2, static::KEYWORDS);
+    }
 
     // static::DELIM;
     public function skener()
@@ -28,7 +34,7 @@ class SkenerController
         //echo(implode(" | ", static::DELIM));
 
         // converted CODE to array
-        $response = $this->codeParser(static::FAKE_CODE);
+        $response = $this->codeParser(static::CODE);
 
         echo "<pre>";
         print_r($response);
@@ -43,23 +49,44 @@ class SkenerController
 
         foreach($code as $char)
         {
-            $inProgress = $inProgress . $char;
+            if($this->isWhitespaceCharacter($char))
+            {
+                // Parbaudit in progress
+                continue;
+            }
+
             
-            if($this->isDelim2($inProgress)) {
-                // TODO": its delim2
-                $response[] = ['symbol' => $char, 'type' => 'delim2'];
-                continue;
-            }
+            $isMatch = $this->isDelimOrKeyword($char);
+            $inProgress = $inProgress . $char;
 
-            if($this->canBeDelim2($inProgress)) 
+            if($this->maybeDelimOrKeyword($inProgress))
             {
                 continue;
             }
 
-            if($this->isDelim($char))
+            $isMatch = $this->isDelimOrKeyword($inProgress);
+
+            if($isMatch)
             {
-                $response[] = ['symbol' => $char, 'type' => 'delim'];
+                $response[] = ['symbol' => $inProgress, 'type' => $isMatch];
+                $inProgress = "";
             }
+
+            // if($this->isDelim2($inProgress)) {
+            //     // TODO": its delim2
+            //     $response[] = ['symbol' => $char, 'type' => 'delim2'];
+            //     continue;
+            // }
+
+            // if($this->canBeDelim2($inProgress)) 
+            // {
+            //     continue;
+            // }
+
+            // if($this->isDelim($char))
+            // {
+            //     $response[] = ['symbol' => $char, 'type' => 'delim'];
+            // }
             
         }
         return $response;
@@ -86,7 +113,46 @@ class SkenerController
             }
 
         }
+        return false;
+    }
+
+    protected function maybeDelimOrKeyword($inProgress): bool
+    {
+        foreach($this->multicharacters as $keywordDelim2)
+        {
+            $isStartingWith = strpos($keywordDelim2, $inProgress) === 0;
+            $isEqual = $inProgress == $keywordDelim2;
+
+            if($isStartingWith && !$isEqual)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected function isDelimOrKeyword($inProgress)
+    {
+        if(in_array($inProgress, static::DELIM))
+        {
+            return "DELIM";
+        }
+
+        if(in_array($inProgress, static::DELIM2))
+        {
+            return "DELIM2";
+        }
+
+        if(in_array($inProgress, static::KEYWORDS))
+        {
+            return "KEYWORD";
+        }
 
         return false;
+    }
+
+    protected function isWhitespaceCharacter($code)
+    {
+        return $code === " ";
     }
 }
